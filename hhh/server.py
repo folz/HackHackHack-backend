ea77d6251c82d588b9648da4a38effdcc9a50bdb
User <user@user-pc>
2012-06-23 13:50:48 -0700

    Add settings.py.dist and import settings.

--- hhh/server.py
+++ hhh/server.py
@@ -1,1 +1,7 @@
-
+try:
+    SETTINGS
+except NameError:
+    try:
+        from settings import *
+    except ImportError:
+        print """Error importing local settings. Have you copied local_settings.py.dist to local_settings.py?"""
